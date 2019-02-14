91200cab4ce65e5344c6aac663138668efefb58e
Nikhil-L <nikhil89515@gmail.com>
2019-02-14 21:33:29 +0530

    Add code for tiny Search Engine

--- Search_Engine.py
+++ Search_Engine.py
@@ -11,7 +11,34 @@ def get_page(url):
 	page = str(BeautifulSoup(response.content, 'lxml'))
 	return page
 
-def get_next_url(page):
+def clean(content):
+	content = TextBlob(content)
+	return content.words
+	
+def crawl_web(seed_page):
+	
+	to_crawl = [seed_page]
+	crawled = []
+	next_depth = []
+	index = {}
+	num = 0
+	while to_crawl and num <= 1:
+		while to_crawl:
+			page = to_crawl.pop()
+			if(page[0:4] == 'http'):
+				num = num + 1
+				break
+		if page not in crawled:
+			content = get_page(page)
+			soup = BeautifulSoup(content, 'lxml')
+			txt = soup.get_text()
+			add_page_to_index(index, page, txt)
+			links = get_all_links(content)
+			union(to_crawl, links)
+			crawled.append(page)
+	return crawled, index
+	
+def get_next_link(page):
 
 	start_link = page.find('<a href=')
 	if start_link == -1:
@@ -21,11 +48,11 @@ def get_next_url(page):
 	url = page[start_quote + 1: end_quote]
 	return url, end_quote
 	
-def get_all_urls(page):
+def get_all_links(page):
 	
 	links = []
 	while True:
-		url, end_pos = get_next_url(page)
+		url, end_pos = get_next_link(page)
 		if url:
 			links.append(url)
 			page = page[end_pos:]
@@ -38,31 +65,35 @@ def union(links, page):
 	for link in page:
 		if link not in links:
 			links.append(link)
+			
+def add_page_to_index(index, url, content):
 	
-def crawl_web(seed_page):
-	
-	to_crawl = [seed_page]
-	crawled = []
-	while to_crawl:
-		while True:
-			page = to_crawl.pop()
-			if(page[0:4] == 'http'):
-				break
-		if page not in crawled:
-			pag = get_page(page)
-			links = get_all_urls(pag)
-			union(to_crawl, links)
-			crawled.append(page)
-			for i in crawled:
-				print (i)
-		else:
-			continue
-	return crawled
+	content = clean(content)
+	for word in content:
+		if len(word) > 5 and len(word) < 15:
+			add_to_index(index, word, url)
+        
+def add_to_index(index, keyword, url):
+    if keyword in index:
+        index[keyword].append(url)
+    else:
+        index[keyword] = [url]
 
+def lookup(index, keyword):
+    if keyword in index:
+        return index[keyword]
+    else:
+        return None
+        
 def main():
-	crawled = crawl_web('https://xkcd.com/')
-	for page in crawled:
-		print (page)
+	crawled, index = crawl_web('https://xkcd.com/')
+	for key in index:
+		print(key)
+	k = raw_input("Enter the keyword you are searching for : ")
+	urls = lookup(index, k)
+	if urls:
+		for url in urls:
+			print(url)
 
 if __name__ == '__main__':
     main()
